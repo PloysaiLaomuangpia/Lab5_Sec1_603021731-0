b5d58b63c9f84712e57e305528f96de1cfc3e6f6
PloysaiLaomuangpia <54529772+PloysaiLaomuangpia@users.noreply.github.com>
2019-09-02 17:30:04 +0700

    Lab5_Sec1_603021731-0

---
+++ app/src/main/java/com/example/lab5intent_603021731_0/Student.kt
@@ -0,0 +1,10 @@
+package com.example.lab5intent_603021731_0
+
+
+import kotlinx.android.parcel.Parcelize
+import android.os.Parcelable as Parcelable1
+
+@Parcelize
+data class Student(val id: String,val name: String, val age: Int): Parcelable1 {
+
+}
